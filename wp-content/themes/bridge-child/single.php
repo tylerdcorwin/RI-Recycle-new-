d20657b4fe9d2e2f81aae4b4e359bb2136ae09e2
Tyler Corwin <Tyler.C@figmints.com>
2024-11-02 14:02:12 -0400

    removing blog header and adjusting for site

--- wp-content/themes/bridge-child/single.php
+++ wp-content/themes/bridge-child/single.php
@@ -3,9 +3,11 @@
 
   <article id="post-<?php the_ID(); ?>" <?php post_class('col-xs-12'); ?>>
     <?php while ( have_posts() ) : the_post(); ?>
-      <div class="ri-blog-header" style="background-image: url('<?php echo get_the_post_thumbnail_url(get_the_id()); ?>')">
+      <div class="ri-blog-header">
+
       </div>
       <div class="ri-post-wrapper container_inner">
+        <img src="<?php echo get_the_post_thumbnail_url(get_the_id()) ?>" alt="blog-featured-image" class="ri-blog-img">
         <h2><?php the_title(); ?></h2>
         <div class="ri-post-details">
           <p>By: <?php the_author(); ?></p>
