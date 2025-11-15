31298a062bd54a28ff6f5099860ee35962109901
Tyler Corwin <Tyler.C@figmints.com>
2025-11-15 13:23:48 -0500

    sync

---
+++ wp-content/themes/figpress-child/blocks/wysiwyg.php
@@ -0,0 +1,13 @@
+<?php
+  $content_area = get_field('content_area');
+  $full_width = get_field('content_full_width');
+?>
+<section class="wysiwyg-content-wrap" data-aos="fade-up">
+  <div class="outer-container">
+    <div class="wysiwyg-content-area <?php echo $full_width ? 'full-width' : ''; ?>">
+      <?php if( $content_area ) { ?>
+        <?php echo $content_area ?>
+      <?php } ?>
+    </div>
+  </div>
+</section>
